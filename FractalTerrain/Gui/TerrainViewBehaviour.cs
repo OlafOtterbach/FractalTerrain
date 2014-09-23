421b014bcd03919ca702e7b6d227447e91334bc8
Olaf <olaf.otterbach@gmx.de>
2014-09-23 22:12:15 +0200

    Resize über Terrainbehaviour.

--- FractalTerrain/Gui/TerrainViewBehaviour.cs
+++ FractalTerrain/Gui/TerrainViewBehaviour.cs
@@ -18,9 +18,18 @@ namespace FractalTerrain.Gui
          var view3D = new TerrainView3DWpf(canvas2D);
          viewModel.View = view3D;
          view3D.ViewModel = viewModel;
+
          terrainCanvas.MouseMove += new System.Windows.Input.MouseEventHandler(view3D.OnMouseMove);
          terrainCanvas.MouseDown += new System.Windows.Input.MouseButtonEventHandler(view3D.OnMouseDown);
-         terrainCanvas.SizeChanged += new SizeChangedEventHandler((object t_sender, SizeChangedEventArgs t_event) => { view3D.Resize(); view3D.Render(); } );
+         terrainCanvas.SizeChanged += new SizeChangedEventHandler( OnResize );
+      }
+
+      private void OnResize(object t_sender, SizeChangedEventArgs t_event)
+      {
+         var parent = Application.Current.MainWindow;
+         var viewModel = parent.DataContext as TerrainViewModel;
+         var view3D = viewModel.View;
+         view3D.Resize(); view3D.Render();
       }
    }
 }
